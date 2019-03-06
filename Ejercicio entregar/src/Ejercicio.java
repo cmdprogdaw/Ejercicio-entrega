5f31b6adb4cf2e053d152fdb000b25f483010755
Cristina <cristinamegi.cmd@gmail.com>
2019-03-06 14:17:37 +0100

    Inicio

---
+++ Ejercicio entregar/src/Ejercicio.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Ejercicio {
+
+	
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String s = new String();
+		System.out.println("Introduce una operacion");
+		s = in.nextLine();
+		Set<Integer> hash = new HashSet<Integer>();
+		for (int i = 0; i<s.length(); i++) {
+			if (i == '+') {
+				 
+			}
+		}
+		System.out.println("El resultado es: " + hash);
+	}
+
+}
